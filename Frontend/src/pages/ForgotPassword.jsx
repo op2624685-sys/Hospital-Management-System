59f44942b1b35289e06ec09bcd98bc20742999e2
Om Prakash <op2624685@gmail.com>
2026-02-16 23:49:06 +0530

    forgetpass UI

--- Frontend/src/pages/ForgotPassword.jsx
+++ Frontend/src/pages/ForgotPassword.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 const ForgotPassword = () => {
   return (
     <div>
-      <h1>FORGOTPASSWORD TITLE</h1>
+      <h1>FORGOTPASSWORD</h1>
+      <form action="">
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" id="email" />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   )
 }
