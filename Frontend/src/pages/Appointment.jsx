a84326d7dff37d08855a9904b2977a271d09660f
Om Prakash <op2624685@gmail.com>
2026-02-18 23:48:10 +0530

    positions setals

--- Frontend/src/pages/Appointment.jsx
+++ Frontend/src/pages/Appointment.jsx
@@ -5,11 +5,12 @@ const Appointment = () => {
   return (
     <div>
       <Header />
-      <div className='section-1'>
+      <div className='flex justify-center items-center'>
+      <div className='section-1 flex flex-col mx-5 my-8 p-6 bg-amber-200'>
         <h1 className='text-3xl'>Reserve Your Spot</h1>
         <p>Secure your preferred time slot by booking in advance. Our simple appointment system allows you to plan ahead and avoid waiting in line, so you can focus on what matters most.</p>
       </div>
-      <div className='section-2'>
+      <div className='section-2 bg-amber-600'>
         <div className='logo'>
 
         </div>
@@ -23,7 +24,7 @@ const Appointment = () => {
             <input type="time" name="time" id="time" />
             <button type="submit">Book Now</button>
           </form>
-
+          </div>
         </div>
       </div>
     </div>
