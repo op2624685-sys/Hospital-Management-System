bfd59d7a8eabcec8819e7fbcc66e861a7fdff62d
Om Prakash <op2624685@gmail.com>
2026-02-19 00:32:42 +0530

    appointment page Done

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -8,13 +8,13 @@ const Header = () => {
       <h1 className='text-3xl font-bold text-orange-400'>DELTACARE</h1>
       <nav className='space-x-10'>
         <RouterLink to="/" className='text-white hover:text-gray-300'>Home</RouterLink>
-        <RouterLink to="/appointment" className='text-white hover:text-gray-300'>Appointment</RouterLink>
-        <RouterLink to="/doctors" className='text-white hover:text-gray-300'>Doctor</RouterLink>
-        <RouterLink to="/branches" className='text-white hover:text-gray-300'>Branches</RouterLink>
-        <RouterLink to="/departments" className='text-white hover:text-gray-300'>Departments</RouterLink>
-        <RouterLink to="/services" className='text-white hover:text-gray-300'>Services</RouterLink>
-        <RouterLink to="/about" className='text-white hover:text-gray-300'>About Us</RouterLink>
-        <RouterLink to="/contact" className='text-white hover:text-gray-300'>Contact Us</RouterLink>
+        <RouterLink to="/appointment" className='text-blue-600 hover:text-gray-300'>Appointment</RouterLink>
+        <RouterLink to="/doctors" className='text-blue-600 hover:text-gray-300'>Doctor</RouterLink>
+        <RouterLink to="/branches" className='text-blue-600 hover:text-gray-300'>Branches</RouterLink>
+        <RouterLink to="/departments" className='text-blue-600 hover:text-gray-300'>Departments</RouterLink>
+        <RouterLink to="/services" className='text-blue-600 hover:text-gray-300'>Services</RouterLink>
+        <RouterLink to="/about" className='text-blue-600 hover:text-gray-300'>About Us</RouterLink>
+        <RouterLink to="/contact" className='text-blue-600 hover:text-gray-300'>Contact Us</RouterLink>
       </nav>
       <RouterLink to="/login" className='bg-orange-500 text-white py-2 px-4 rounded-lg hover:bg-orange-600'><LogIn />Login</RouterLink>
     </header>
--- Frontend/src/pages/Appointment.jsx
+++ Frontend/src/pages/Appointment.jsx
@@ -3,27 +3,34 @@ import Header from '../components/Header'
 
 const Appointment = () => {
   return (
-    <div>
+    <div className=' p-5'>
       <Header />
       <div className='flex justify-center items-center m-10 px-30'>
-      <div className='section-1 flex flex-col m-10 p-6 h-150 w-1/2 bg-amber-200'>
-        <h1 className='text-3xl'>Reserve Your Spot</h1>
-        <p>Secure your preferred time slot by booking in advance. Our simple appointment system allows you to plan ahead and avoid waiting in line, so you can focus on what matters most.</p>
-      </div>
-      <div className='section-2 bg-amber-600'>
-        <div className='logo'>
-
+        <div className='section-1 flex flex-col m-10 p-12 h-150 w-1/2 '>
+          <h1 className='text-8xl font-semibold m-3 px-2'>Reserve Your Spot</h1>
+          <p className='text-2xl m-3 mt-10'>Secure your preferred time slot by booking in advance. Our simple appointment system allows you to plan ahead and avoid waiting in line, so you can focus on what matters most.</p>
         </div>
-        <div className='form'>
-          <form className=''>
-            <label htmlFor="doctor">Doctor name</label>
-            <input type="text" name="doctor" id="doctor" />
-            <label htmlFor="date">Patient name</label>
-            <input type="date" name="date" id="date" />
-            <label htmlFor="time">Date & Time</label>
-            <input type="time" name="time" id="time" />
-            <button type="submit">Book Now</button>
-          </form>
+
+        <div className='section-2 w-1/3 h-150 m-10 p-12'>
+          <div className='logo flex justify-center items-center mb-15 '>
+              <img className='h-30 w-30 rounded-full' src="https://images.unsplash.com/photo-1551601651-2a8555f1a136?q=80&w=847&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
+          </div>
+          <div className='form relative'>
+            <form className='flex flex-col'>
+              <label className='relative text-sm duration-300 transform -translate scale-75 top-3 -z-10 origin-left peer-focus:text-blue-500' htmlFor="doctor">Doctor name</label>
+              <input className='block w-full py-2.5 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:text-white focus:border-blue-500 peer' type="text" name="doctor" id="doctor" />
+
+              <label className='relative text-sm duration-300 transform -translate scale-75 top-3 -z-10 origin-left peer-focus:text-blue-500' htmlFor="doctor">Patient name</label>
+              <input className='block w-full py-2.5 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:text-white focus:border-blue-500 peer' type="text" name="patient" id="patient" />
+
+              <label className='relative text-sm duration-300 transform -translate scale-75 top-3 -z-10 origin-left peer-focus:text-blue-500' htmlFor="date">Date</label>
+              <input className='block w-full py-2.5 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:text-white focus:border-blue-500 peer' type="date" name="date" id="date" />
+
+              <label className='relative text-sm duration-300 transform -translate scale-75 top-3 -z-10 origin-left peer-focus:text-blue-500' htmlFor="time">Time</label>
+              <input className='block w-full py-2.5 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:text-white focus:border-blue-500 peer' type="time" name="time" id="time" />
+
+              <button className='w-full mb-4 mt-6 text-[18px] rounded bg-blue-500 py-2 hover:bg-blue-600 transition-colors duration-300 active:scale-90' type="submit">Book Now</button>
+            </form>
           </div>
         </div>
       </div>
