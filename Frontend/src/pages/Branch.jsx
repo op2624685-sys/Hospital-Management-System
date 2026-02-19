20d898d2cc2edcde780d6d03ba3e8190b4684eeb
Om Prakash <op2624685@gmail.com>
2026-02-20 00:30:15 +0530

    branch page

--- Frontend/src/pages/Branch.jsx
+++ Frontend/src/pages/Branch.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import Header from '../components/Header'
+import { Search } from 'lucide-react'
 
 const Branch = () => {
   return (
     <div className='mt-20 px-20 py-5'>
       <Header />
-      <div>
-        
+      <div className='flex justify-center items-center mt-20 px-30'>
+        <div className='section-1 flex flex-col m-10 p-8 h-150 w-1/2 bg-green-300 '>
+          <h2 className='text-8xl font-semibold mb-2'>Find My Branch Near You</h2>
+          <p className='text-xl mt-6'>Locate our hospitals across the country with ease. View brach addresses, contact, information and operating hours to plan your visit.</p>
+        </div>
+        <div className='section-2 m-10 p-8 h-150 w-2/3 bg-red-300'>
+          <form className='flex justify-between py-1 px-4 border-2 rounded-2xl shadow-md shadow-black border-black w-md'>
+            <input className='block w-full py-2 px-0 text-md text-black bg-transparent border-0 border-b-2 border-gray-600 appearance-none focus:outline-none focus:ring-0 focus:text-gray-800 focus:border-gray-800 peer' type="search" id="branch_search" name="brach_search" placeholder="Search for a branch...." />
+            <button className='cursor-pointer active:scale-80' type="submit"><Search /></button>
+          </form>
+          
+
+         
+
+        </div>
       </div>
     </div>
   )
