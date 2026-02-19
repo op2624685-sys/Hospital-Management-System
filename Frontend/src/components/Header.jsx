68c78fa140b232dada2291fd3529a5e5a9c64668
Om Prakash <op2624685@gmail.com>
2026-02-19 19:55:28 +0530

    added gsap animation in home

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React  from 'react';
 import { LogIn } from 'lucide-react';
 import { Link as RouterLink } from 'react-router-dom';
 
 const Header = () => {
+
   return (
-    <header className='flex justify-between top-0 left-0 fixed w-full items-center py-6 px-10'>
+    <header className='navbar flex justify-between top-0 left-0 fixed w-full items-center py-6 px-10'>
       <h1 className='text-3xl font-bold text-orange-400'>DELTACARE</h1>
       <nav className='space-x-10'>
         <RouterLink to="/" className='text-blue-600 hover:text-gray-300'>Home</RouterLink>
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,18 +1,65 @@
-import React from 'react';
+import React, { useLayoutEffect, useRef } from 'react';
+import { gsap } from 'gsap';
 import Header from '../components/Header';
 import MainContent from '../components/MainContent';
 import Appointment from '../pages/Appointment';
 import Doctor from '../pages/Doctor';
 
 const Home = () => {
+
+  const container = useRef();
+  const zoomIn = useRef();
+
+  // this was for zoom in content
+  useLayoutEffect(() => {
+    const ctx = gsap.context(() => {
+
+      gsap.from(".animate-item", {
+        scale: 0,
+        opacity: 0,
+        transformOrigin: "center center",
+        duration: 1,
+        ease: "power3.out"
+      });
+
+    }, zoomIn);
+
+    return () => ctx.revert();
+  }, []);
+
+  // this is navbar animation
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      gsap.from(".navbar", {
+        opacity: 0,
+        y: -100,
+        duration: 0.8,
+        delay: 0.6,
+        stagger: 1,
+        onStart: () => {
+        }
+      });
+    }, container);
+
+    return () => ctx.revert();
+  }, []);
+
   return (
-    <div className=' min-h-screen flex flex-col  '>
-        <img className='fixed h-screen w-full top-0 right-0 opacity-70' src="https://i.pinimg.com/736x/0e/35/f0/0e35f02109614b1eed3422055c9307d6.jpg" />
-      <div className='relative'>
-      <Header />
-      <MainContent />
-      <Appointment />
-      <Doctor />
+    <div className='min-h-screen flex flex-col'>
+
+      <img
+        className='fixed h-screen w-full top-0 right-0 opacity-70'
+        src="https://i.pinimg.com/736x/0e/35/f0/0e35f02109614b1eed3422055c9307d6.jpg"
+        alt=""
+      />
+
+      <div ref={zoomIn} className='relative min-h-screen will-change-transform'>
+        <div className='animate-item'>
+          <div ref={container} className='navbar'>
+            <Header />
+          </div>
+          <MainContent />
+        </div>
       </div>
     </div>
   );
