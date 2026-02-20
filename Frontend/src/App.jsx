b1a83ce103165e63e139de45c5cd4ec0a64eb9b0
Om Prakash <op2624685@gmail.com>
2026-02-20 20:14:12 +0530

    routing

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -9,6 +9,12 @@ import Appointment from './pages/Appointment'
 import Doctor from './pages/Doctor'
 import Branch from './pages/Branch'
 import Department from './pages/Department'
+import EmergencyDepartment from './components/EmergencyDepartment'
+import Cardiology from './components/Cardiology'
+import Neurology from './components/Neurology'
+import Orthopedics from './components/Orthopedics'
+import Pediatrics from './components/Pediatrics'
+import Radiology from './components/Radiology'
 
 const App = () => {
   return (
@@ -18,7 +24,15 @@ const App = () => {
       <Route path='/appointment' element={<Appointment />} />
       <Route path='/doctors' element={<Doctor />} />
       <Route path='/branches' element={<Branch />} />
+      {/* departments page link */}
       <Route path='departments' element={<Department />} />
+      <Route path='departments/emergency' element={<EmergencyDepartment />} />
+      <Route path='departments/cardiology' element={<Cardiology />} />
+      <Route path='departments/neurology' element={<Neurology />} />
+      <Route path='departments/orthopedics' element={<Orthopedics />} />
+      <Route path='departments/pediatrics' element={<Pediatrics />} />
+      <Route path='departments/radiology' element={<Radiology />} />
+      {/* Auth pages link */}
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path='/login/forgotpassword' element={<ForgotPassword />} />
