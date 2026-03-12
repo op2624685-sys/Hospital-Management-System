a14d47a8e94f020f1acdce740d1c06bf542e9628
Om Prakash <op2624685@gmail.com>
2026-03-12 22:35:57 +0530

    doctors have write pessimistic rights

--- Backend/src/main/java/com/hms/repository/DoctorRepository.java
+++ Backend/src/main/java/com/hms/repository/DoctorRepository.java
@@ -1,9 +1,12 @@
 package com.hms.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Lock;
+import jakarta.persistence.LockModeType;
 
 import com.hms.entity.Doctor;
 
@@ -13,4 +16,7 @@ public interface DoctorRepository extends JpaRepository<Doctor, Long> {
     List<Doctor> findByName(String name);
 
     List<Doctor> findByBranch_Id(Long branchId);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<Doctor> findByIdForUpdate(Long id);
 }
