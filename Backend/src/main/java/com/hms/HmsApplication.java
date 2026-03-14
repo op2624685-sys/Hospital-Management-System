88dabb61753efe6eb2c087aa3a4a4e17ba05e208
Om Prakash <op2624685@gmail.com>
2026-03-15 01:32:49 +0530

    enableJpaRepositories in main class

--- Backend/src/main/java/com/hms/HmsApplication.java
+++ Backend/src/main/java/com/hms/HmsApplication.java
@@ -2,8 +2,10 @@ package com.hms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.hms.repository")
 public class HmsApplication {
 
 	public static void main(String[] args) {
