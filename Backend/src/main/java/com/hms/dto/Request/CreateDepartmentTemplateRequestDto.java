377ae7735cfe361ca04dcb505c60dfc5641bb826
Om Prakash <op2624685@gmail.com>
2026-03-22 23:27:48 +0530

    create department templete request dto

---
+++ Backend/src/main/java/com/hms/dto/Request/CreateDepartmentTemplateRequestDto.java
@@ -0,0 +1,35 @@
+package com.hms.dto.Request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateDepartmentTemplateRequestDto {
+
+    @NotBlank(message = "name is required")
+    @Size(max = 100, message = "name must be at most 100 characters")
+    private String name;
+
+    @Size(max = 2000, message = "description must be at most 2000 characters")
+    private String description;
+
+    @Size(max = 2000, message = "imageUrl must be at most 2000 characters")
+    private String imageUrl;
+
+    @Size(max = 20, message = "accentColor must be at most 20 characters")
+    private String accentColor;
+
+    @Size(max = 20, message = "bgColor must be at most 20 characters")
+    private String bgColor;
+
+    @Size(max = 20, message = "icon must be at most 20 characters")
+    private String icon;
+
+    @Size(max = 10000, message = "sectionsJson must be at most 10000 characters")
+    private String sectionsJson;
+}
