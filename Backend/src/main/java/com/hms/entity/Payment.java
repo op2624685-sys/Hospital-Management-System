2fb09ba0d27e4b604c4540eeaf68f524e09cac5b
Om Prakash <op2624685@gmail.com>
2026-03-23 23:06:27 +0530

    create the payment entity with relationship of appointment ManyTOOne

---
+++ Backend/src/main/java/com/hms/entity/Payment.java
@@ -0,0 +1,52 @@
+package com.hms.entity;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.Id;
+
+import com.hms.entity.type.PaymentStatus;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "payments")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String paymentIntentId;   // pi_xxx from Stripe
+    
+    private Long amount;               // in paise
+
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus status;             // succeeded / failed
+    
+    @ManyToOne
+    @JoinColumn(name = "appointment_id", nullable = false)
+    private Appointment appointment;
+
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
