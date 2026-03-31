758599782fef0a6bea6b8613e891be729035bbe7
Om Prakash <op2624685@gmail.com>
2026-03-31 22:07:33 +0530

    in app ocnfig create the bean of the objectMapper class which return jsonMapper this is for store data in redis cache

--- Backend/src/main/java/com/hms/config/AppConfig.java
+++ Backend/src/main/java/com/hms/config/AppConfig.java
@@ -1,6 +1,10 @@
 package com.hms.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,6 +14,15 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class AppConfig {
+
+    @Bean
+    @Primary
+    public ObjectMapper objectMapper() {
+        return JsonMapper.builder()
+                .findAndAddModules()
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                .build();
+    }
 
     @Bean
     public ModelMapper modelMapper() {
