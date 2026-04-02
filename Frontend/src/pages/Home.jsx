f6632180b61774fc4c53d3fd936ff95d168d6cff
Om Prakash <op2624685@gmail.com>
2026-04-02 23:03:02 +0530

    changes the style of the footer of home page

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -35,7 +35,7 @@ const Home = () => {
 
       {/* Footer note */}
       <div className='text-center pb-10' style={{ zIndex: 1, position: 'relative' }}>
-        <p className='text-xs' style={{ color: '#646464' }}>
+        <p className='text-xs' style={{ color: 'var(--muted-foreground)' }}>
           © 2026 DELTACARE Hospital · All rights reserved
         </p>
       </div>
