d6302322236887e1dceaded93faeed98245bfe0c
Om Prakash <op2624685@gmail.com>
2026-04-20 23:48:34 +0530

    react query client configration

---
+++ Frontend/src/lib/queryClient.js
@@ -0,0 +1,14 @@
+import { QueryClient } from '@tanstack/react-query'
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 10 * 60 * 1000,
+      gcTime: 30 * 60 * 1000,
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      retry: 1,
+    },
+  },
+})
