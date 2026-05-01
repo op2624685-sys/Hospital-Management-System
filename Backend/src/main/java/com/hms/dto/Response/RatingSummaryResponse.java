18ac9a44185af33ed8634f145f6bacf34df87801
Om Prakash <op2624685@gmail.com>
2026-05-01 23:04:58 +0530

    implements serializable in ratingSummary response

--- Backend/src/main/java/com/hms/dto/Response/RatingSummaryResponse.java
+++ Backend/src/main/java/com/hms/dto/Response/RatingSummaryResponse.java
@@ -1,7 +1,9 @@
 package com.hms.dto.Response;
 
+import java.io.Serializable;
+
 public record RatingSummaryResponse(
         Long   doctorId,
         double averageRating,
         long   totalReviews
-) {}
+) implements Serializable {}
