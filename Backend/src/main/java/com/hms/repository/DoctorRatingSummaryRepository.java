d847f8d65c49664ec561c3abee414e6565cc237d
Om Prakash <op2624685@gmail.com>
2026-05-01 23:05:36 +0530

    find rating summary by doctor id

--- Backend/src/main/java/com/hms/repository/DoctorRatingSummaryRepository.java
+++ Backend/src/main/java/com/hms/repository/DoctorRatingSummaryRepository.java
@@ -1,8 +1,11 @@
 package com.hms.repository;
 
 import com.hms.entity.DoctorRatingSummary;
+import java.util.Collection;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DoctorRatingSummaryRepository extends JpaRepository<DoctorRatingSummary, Long> {
     // findById(doctorId) inherited from JpaRepository
+    List<DoctorRatingSummary> findByDoctorIdIn(Collection<Long> doctorIds);
 }
