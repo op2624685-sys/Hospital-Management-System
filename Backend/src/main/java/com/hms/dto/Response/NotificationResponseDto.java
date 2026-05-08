1c073ae23377cb898dfbac87869f37edcec8c343
Om Prakash <op2624685@gmail.com>
2026-05-08 18:00:38 +0530

    create the notificationResponseDto

---
+++ Backend/src/main/java/com/hms/dto/Response/NotificationResponseDto.java
@@ -0,0 +1,24 @@
+package com.hms.dto.Response;
+
+import com.hms.entity.type.NotificationType;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class NotificationResponseDto {
+    private Long id;
+    private NotificationType type;
+    private String title;
+    private String message;
+    private String appointmentId;
+    private boolean read;
+    private LocalDateTime createdAt;
+}
