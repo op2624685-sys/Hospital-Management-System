9f9bc5134c884deb6c7f7f923b645122c0e8f08b
Om Prakash <op2624685@gmail.com>
2026-05-08 18:00:58 +0530

    create the unreadCountResponseDto

---
+++ Backend/src/main/java/com/hms/dto/Response/UnreadCountResponseDto.java
@@ -0,0 +1,14 @@
+package com.hms.dto.Response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UnreadCountResponseDto {
+    private long unreadCount;
+}
