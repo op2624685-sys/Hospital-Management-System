d4be0a7b01a3b27282a474976249e97a58c870d7
Om Prakash <op2624685@gmail.com>
2026-05-27 15:49:37 +0530

    create the doctoStampResponseDto for getting the DoctorStampUrl from cloudinary

---
+++ Backend/src/main/java/com/hms/dto/Response/DoctorStampResponseDto.java
@@ -0,0 +1,12 @@
+package com.hms.dto.Response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DoctorStampResponseDto {
+    private String doctorStampUrl;
+}
