37b15251a5cd542c5c6ca6d008dc2f3a008a70f2
Om Prakash <op2624685@gmail.com>
2026-05-28 16:37:32 +0530

    reslove the Null value passed in findById with @NonNull annotation

--- Backend/src/main/java/com/hms/repository/ReceptionistRepository.java
+++ Backend/src/main/java/com/hms/repository/ReceptionistRepository.java
@@ -4,12 +4,14 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 
 import com.hms.entity.Receptionist;
 
 public interface ReceptionistRepository extends JpaRepository<Receptionist, Long> {
 
     @EntityGraph(attributePaths = {"branch", "department", "user"})
+    @NonNull
     Optional<Receptionist> findById(Long id);
 
     boolean existsByDepartment_Id(Long departmentId);
