95cac7301cf3e0a35d8387f41e28feed47d80657
Om Prakash <op2624685@gmail.com>
2026-05-30 21:00:54 +0530

    create the custom constructor of DoctorResponseDto

--- Backend/src/main/java/com/hms/dto/Response/DoctorResponseDto.java
+++ Backend/src/main/java/com/hms/dto/Response/DoctorResponseDto.java
@@ -4,15 +4,23 @@ import java.io.Serializable;
 import java.util.Set;
 
 import com.hms.dto.DepartmentDto;
+
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
+@Getter
+@Setter
 @NoArgsConstructor
-public class DoctorResponseDto implements Serializable{
+@AllArgsConstructor
+public class DoctorResponseDto implements Serializable {
     
+    public DoctorResponseDto(Long id2, String name2, String specialization2, String email2,
+            Set<DepartmentDto> mapDepartments, BranchResponseDto mapBranchResponse, Long consultationFee2,
+            Object object, String doctorStampUrl2) {
+
+    }
     private Long id;
     private String name;
     private String specialization;
@@ -22,4 +30,7 @@ public class DoctorResponseDto implements Serializable{
     private Long consultationFee;
     private String profilePhoto;
     private String doctorStampUrl;
+    private Double averageRating = 0.0;
+    private Long totalReviews = 0L;
+
 }
