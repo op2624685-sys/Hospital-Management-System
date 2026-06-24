0a76c917957c795e8e90b0542bdf9ce2b0664e2a
Om Prakash <op2624685@gmail.com>
2026-06-24 14:42:03 +0530

    remove the strict mode of react

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -9,7 +9,6 @@ import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from './lib/queryClient'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <AuthProvider>
@@ -19,6 +18,5 @@ createRoot(document.getElementById('root')).render(
         </AuthProvider>
       </BrowserRouter>
     </QueryClientProvider>
-  </StrictMode>
 
 )
